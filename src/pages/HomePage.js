129f8989c25663424f310cecb34db50ac47d24f6
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-27 21:26:03 +0700

    change severity: danger => error

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -61,7 +61,7 @@ const HomePage = () => {
     <Container>
       <Stack sx={{ display: "flex", alignItems: "center", m: "2rem" }}>
         <Typography variant="h3" sx={{ textAlign: "center" }}>Book Store</Typography>
-        {errorMessage && <Alert severity="danger">{errorMessage}</Alert>}
+        {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
         <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
           <Stack
             spacing={2}
