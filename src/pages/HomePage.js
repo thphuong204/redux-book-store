8039dad1eae6bec9fe1d4325354805a3a05e9ebc
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-28 12:52:52 +0700

    import function fetchbook from bookSlice, import useDistpatch, useSelector from react-redux, passing prop - pageNum, limit, query through function fetchBook to dispatch action to change state in store.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,49 +3,35 @@ import { ClipLoader } from "react-spinners";
 import { useNavigate } from "react-router-dom";
 import PaginationBar from "../components/PaginationBar";
 import SearchForm from "../components/SearchForm";
-import api from "../apiService";
 import { FormProvider } from "../form";
 import { useForm } from "react-hook-form";
 import { Container, Alert, Box, Card, Stack, CardMedia, CardActionArea, Typography, CardContent } from "@mui/material";
-
+import { fetchBook } from "../features/bookSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 
 const BACKEND_API = process.env.REACT_APP_BACKEND_API;
 
 const HomePage = () => {
-  const [books, setBooks] = useState([]);
   const [pageNum, setPageNum] = useState(1);
   const totalPage = 10;
   const limit = 10;
-
-  const [loading, setLoading] = useState(false);
   const [query, setQuery] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+
+  const dispatch = useDispatch();
+  const {books,isLoading, errorMessage } = useSelector((state) => state.book) 
+  
 
   const navigate = useNavigate()
   const handleClickBook = (bookId) => {
     navigate(`/books/${bookId}`);
   };
 
-
-
-
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        let url = `/books?_page=${pageNum}&_limit=${limit}`;
-        if (query) url += `&q=${query}`;
-        const res = await api.get(url);
-        setBooks(res.data);
-        setErrorMessage("");
-      } catch (error) {
-        setErrorMessage(error.message);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, [pageNum, limit, query]);
+    dispatch(fetchBook(pageNum,limit,query)); 
+  }, [pageNum, limit, query, dispatch]);
+
+
   //--------------form
   const defaultValues = {
     searchQuery: ""
@@ -80,13 +66,13 @@ const HomePage = () => {
         />
       </Stack>
       <div>
-        {loading ? (
+        {isLoading ? (
           <Box sx={{ textAlign: "center", color: "primary.main" }} >
             <ClipLoader color="inherit" size={150} loading={true} />
           </Box>
         ) : (
           <Stack direction="row" spacing={2} justifyContent="space-around" flexWrap="wrap">
-            {books.map((book) => (
+            {books?.map((book) => (
               <Card
                 key={book.id} onClick={() => handleClickBook(book.id)}
                 sx={{
