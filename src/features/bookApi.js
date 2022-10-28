e616e50b488fffdba80e03866b28f97af78e499b
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-28 13:18:48 +0700

    return back to right url after testing handling error

--- src/features/bookApi.js
+++ src/features/bookApi.js
@@ -2,7 +2,7 @@ import api from "../apiService";
 
 export  const fetchData = async (pageNum,limit,query) => {
     
-      let url = `/boks?_page=${pageNum}&_limit=${limit}`;
+      let url = `/books?_page=${pageNum}&_limit=${limit}`;
       if (query) url += `&q=${query}`;
       const res = await api.get(url);
       return res;
